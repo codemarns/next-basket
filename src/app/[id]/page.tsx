86e6254e78dc1ecba8a0f5d831c5bf6933c23153
Marnien Cueba <marniencueba94@gmail.com>
2024-02-04 22:26:34 +0800

    empty Product page

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+
+export default function Product() {
+  return (
+    <>
+      {/* CONTENT */}
+      <div className="py-4">
+        
+      </div>
+    </>
+  );
+}
